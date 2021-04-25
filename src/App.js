44ad3575920f43bca1590cdc4687f0aeae02f8ff
blue-core <taretw567@gmail.com>
2021-04-25 15:17:23 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,55 @@
+import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
+import { Dimmer, Loader } from 'semantic-ui-react';
+import Weather from './components/weather';
+import './App.css';
+/*
+await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+*/
+function App() {
+
+  const [lat, setLat] = useState([]);
+  const [long, setLong] = useState([]);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      navigator.geolocation.getCurrentPosition(function(position) {
+        setLat(position.coords.latitude);
+        setLong(position.coords.longitude);
+      });
+
+      await fetch(`${process.env.REACT_APP_API_URL}/weather/?lat=${lat}&lon=${long}&units=metric&APPID=${process.env.REACT_APP_API_KEY}`)
+      .then(res => res.json())
+      .then(result => {
+        setData(result)
+        console.log(result);
+      });
+    }
+    fetchData();
+  }, [lat,long])
+
+
+  return (
+    <div className="App">
+
+      {(typeof data.main != 'undefined') ? (
+        <Weather weatherData={data}/>
+      ) : (
+        <div>
+          <Dimmer active>
+             <Loader>Loading...</Loader>
+          </Dimmer>
+        </div>
+      )}
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/weather.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import './styles.css';
+import { Card } from 'semantic-ui-react';
+import moment from 'moment';
+import { Button } from 'semantic-ui-react';
+
+const refresh = () => {
+    window.location.reload();
+}
+
+const CardExampleCard = ({weatherData}) => (
+
+    <div className="main">
+
+          <div className="top">
+          <p className="header">{weatherData.name}</p>
+          <Button className="button" inverted color='blue' circular icon='refresh' onClick={refresh} />
+          </div>
+
+          <div className="flex">
+          <p className="day">Day: {moment().format('dddd')}</p>
+          <p className="date">Date: {moment().format('LL')}</p>
+          </div>
+
+          
+        <div className="style">
+          <div className="flex">
+          <p className="temp">Temperature: {weatherData.main.temp} &deg;C</p>
+          <p className="desc">Description: {weatherData.weather[0].description}</p>
+          </div>
+
+       
+          <div className="flex">
+          <p className="sunrise">Sunrise: {new Date(weatherData.sys.sunrise * 1000).toLocaleTimeString('en-IN')}</p>
+          <p className="sunset">Sunset: {new Date(weatherData.sys.sunset * 1000).toLocaleTimeString('en-IN')}</p>
+          </div>
+
+          
+          <div className="flex">
+          <p className="wind-speed">Wind speed: {weatherData.wind.speed} m/s</p>
+          <p className="humid">Humidity: {weatherData.main.humidity} %</p>
+          </div>
+        </div>
+        
+    </div>
+)
+
+export default CardExampleCard;
